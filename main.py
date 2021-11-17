302122f55765aaf07418b78e5e5f1394a395accd
Francis Rousseau <frr@mybingli.com>
2021-11-17 22:17:12 +0100

    test credentials

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Application coffeeAPI loaded succesfully</p>"
+
+
+@app.route("/consumptions")
+def consumptions():
+    user = os.getenv('credential.user', 'userX')
+    return user
