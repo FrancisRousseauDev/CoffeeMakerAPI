beda5f746c0d8ee112f56722f907408a42adb8ef
Francis Rousseau <frr@mybingli.com>
2021-11-17 22:33:12 +0100

    return select statement from consumptions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 import os
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
@@ -11,5 +11,30 @@ def hello_world():
 
 @app.route("/consumptions")
 def consumptions():
-    user = os.getenv('credential.user', 'userX')
-    return user
+    records = readDatabase()
+    return jsonify(records)
+
+
+def readDatabase():
+    try:
+        connection = psycopg2.connect(user=os.getenv('credential.user'),
+                                      password=os.getenv('credential.password'),
+                                      host=os.getenv('credential.host'),
+                                      port=os.getenv('credential.port'),
+                                      database=os.getenv('credential.database'))
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "select * from consumption"
+
+        cursor.execute(postgreSQL_select_Query)
+        mobile_records = cursor.fetchall()
+        return mobile_records
+
+
+    except (Exception, psycopg2.Error) as error:
+        return str(error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
